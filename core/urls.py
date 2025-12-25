a2558f84f51e7971ee8e4857e7999e446468dd6c
dat234 <datviet0203@gmail.com>
2025-12-25 13:49:36 +0700

    Setup Deploy and Google Login

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,8 @@
 # core/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include # <-- Thêm include
 from django.conf import settings
 from django.conf.urls.static import static
-
 
 from store.views import (
     home, product_detail, add_to_cart, cart_view, 
@@ -11,15 +10,21 @@ from store.views import (
     order_success,
     landing_page
 )
-# THÊM IMPORT MỚI TỪ APP 'users'
+
 from users.views import (
     register_view, login_view, logout_view, 
-    order_history_view  ,
+    order_history_view,
     order_detail_view
 )
 from store.admin import my_admin_site
+
 urlpatterns = [
-    path('admin/', my_admin_site.urls),  # Sử dụng custom admin site
+    path('admin/', my_admin_site.urls),
+    
+    # --- ĐƯỜNG DẪN CHO GOOGLE LOGIN ---
+    path('accounts/', include('allauth.urls')), 
+    
+    # Store URLs
     path('home/', home, name='home'),
     path('', landing_page, name='landing_page'),
     path('product/<int:product_id>/', product_detail, name='product_detail'),
@@ -28,7 +33,8 @@ urlpatterns = [
     path('update-cart/<int:product_id>/', update_cart, name='update_cart'),
     path('checkout/', checkout, name='checkout'),
     path('order-success/', order_success, name='order_success'),
-    # URL cho user registration, login, logout
+    
+    # User URLs
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
